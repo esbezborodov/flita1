e565cbe405f79b8b34b13a3808c924f55f255163
esbezborodov <114301110+esbezborodov@users.noreply.github.com>
2023-03-21 23:57:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-int strSet(set <string> set) {
+int strSet2(set <string> set) {
     char oper;
 
     while (oper != '!') {
@@ -36,28 +36,28 @@ int strSet(set <string> set) {
 }
 
 
-int numSet(set <int> set) {
+int strSet1(set <string> set) {
     char oper;
 
     while (oper != '!') {
-        cout << "enter '+' for add set element or '-' for remove set element(numbers)\nenter '!' to stop\n";
+        cout << "enter '+' for add set element or '-' for remove set element(strings)\nenter '!' to stop\n";
         cin >> oper;
         switch (oper) {
-            case '+' :
-                int num1;
-                cout << "enter a number:\n";
-                cin >> num1;
-                set.insert(num1);
+            case '-' : {
+                string str2;
+                cout << "enter an element \n";
+                cin >> str2;
+                set.erase(str2);
                 cout << "set elements:\n";
                 for(auto i : set)
                     cout << i << endl;
                 break;
-
-            case '-' :
-                int num2;
-                cout << "enter a number:\n";
-                cin >> num2;
-                set.erase(num2);
+            }
+            case '+' :
+                string str1;
+                cout << "enter an element:\n";
+                cin >> str1;
+                set.insert(str1);
                 cout << "set elements:\n";
                 for(auto j : set)
                     cout << j << endl;
@@ -69,21 +69,23 @@ int numSet(set <int> set) {
 
 
 int main() {
-    set <string> str_set;
-    set <int> num_set;
+    set<string> first_set;
+    set<string> second_set;
 
-     {
+    {
         int action;
-        cout << "enter '1' for work with strings or '2' for work with numbers:\n";
-        cin >> action;
+        while (1) {
+            cout << "enter '1' for work with first set or '2' for work with second set:\n";
+            cin >> action;
 
-        switch (action) {
-            case 1:
-                strSet(str_set);
-                break;
-            case 2:
-                numSet(num_set);
-                break;
+            switch (action) {
+                case 1:
+                    strSet2(second_set);
+                    break;
+                case 2:
+                    strSet1(first_set);
+                    break;
+            }
         }
     }
 }
